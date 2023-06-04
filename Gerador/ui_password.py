5b2bd2b6637ed53d6fc9dae1334eaf7839a2b450
Vinícius Marcelo <ermelotp@outlook.com>
2023-06-04 15:31:45 -0300

    :3

--- Gerador/ui_password.py
+++ Gerador/ui_password.py
@@ -3,8 +3,6 @@ from tkinter import *
 from tkinter import ttk
 
 def ui():
-
-
     # Cria a instância root
     root = Tk()
     root.title("Password Generator")
@@ -12,17 +10,14 @@ def ui():
     frame = ttk.Frame(root, padding=20)
     # "Transforma" o frame em grid para facilitar alocação
     frame.grid()
-    # Cria um label no frame e usa .grid para organizar
-    ttk.Label(frame, textvariable=text()[0]).grid(column=0, row=0)
-    ttk.Label(frame, textvariable=text()[1]).grid(column=0, row=1)
-    ttk.Label(frame, textvariable=text()[2]).grid(column=0, row=2)
-    ttk.Button(frame, text="Quit", command=text()).grid(column=0, row=3)
-    root.mainloop()
 
-def text():
-    #Armazena as 3 senhas em passwords (tuples)
-    passwords = generator()
-    return passwords
+    # Cria um label no frame e usa .grid para organizar
+    ttk.Label(frame, text=generator()[0]).grid(column=0, row=0)
+    ttk.Label(frame, text=generator()[1]).grid(column=0, row=1)
+    ttk.Label(frame, text=generator()[2]).grid(column=0, row=2)
+    ttk.Button(frame, text="Quit", command=root.destroy).grid(column=0, row=3)
+
+    root.mainloop()
 
 # Garante a inicialização caso este seja o código principal
 if(__name__=="__main__"):
