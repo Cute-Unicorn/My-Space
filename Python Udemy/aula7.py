09629a2093b3acb3ee89ba170f0ebbc5c5a82b3b
Vinícius Marcelo <ermelotp@outlook.com>
2023-06-05 21:03:24 -0300

    Atualização do progresso no curso de Python

---
+++ Python Udemy/aula7.py
@@ -0,0 +1,10 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar
+# números e underline _.
+# O sinal de = é um operador de atribuição. Ele é usado para
+# atribuir um valor a um nome (variável).
+# Uso: nome_variavel = expressão
+nome_completo = "Vinícus Marcelo R. E."
+print('Nome:',nome_completo)
+
+maior_idade = 30 >= 18
