2fdd63f2137bdf2656747aaa8528a408da502d86
Vinícius Marcelo <ermelotp@outlook.com>
2023-06-16 15:18:58 -0300

    Inclusão das aulas 33 a 36

---
+++ Python Udemy/aula33.py
@@ -0,0 +1,7 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+"""
+string = 'Vinícius Marcelo'
+# string[3] = 'XYZ'
+print(string.capitalize())
---
+++ Python Udemy/aula34.py
@@ -0,0 +1,9 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+"""
+while True:
+    nome = input('Qual seu nome? ')
+    print(f'Seu nome é {nome}')
+    break
